024cc27712842c6398ee2467a0f11ec090c8c695
Ordenkrieger <OrdenKriegerEDR@googlemail.com>
2016-08-04 00:48:20 +0200

    Revert "ham: Remove leftovers"
    This reverts commit 9090f4ed9777746b3a292525c027e7c72deca504.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -117,7 +117,6 @@ WIFI_DRIVER_FW_PATH_STA          := "sta"
 WIFI_DRIVER_FW_PATH_AP           := "ap"
 TARGET_USES_WCNSS_CTRL           := true
 TARGET_USES_QCOM_WCNSS_QMI       := true
-TARGET_PROVIDES_WCNSS_QMI        := true
 TARGET_USES_WCNSS_MAC_ADDR_REV   := true
 
 # Lights
