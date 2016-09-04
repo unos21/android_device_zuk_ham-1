7f8240d4e3edc5b137a84df2c07ce156a95fbf11
Ordenkrieger <OrdenKriegerEDR@googlemail.com>
2016-09-04 23:59:29 +0200

    ham: Remove last ANT+ stuff, cm stuff

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -50,9 +50,6 @@ TARGET_KERNEL_SOURCE := kernel/cyanogen/msm8974
 TARGET_KERNEL_CONFIG := chroma_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
 
-# ANT+
-BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
-
 # Enable DIAG on debug builds
 ifneq ($(TARGET_BUILD_VARIANT),user)
 TARGET_KERNEL_ADDITIONAL_CONFIG:= cyanogenmod_debug_config
