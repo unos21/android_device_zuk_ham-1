28fdf623d8b9de7ffc8d9b122dfc01bf01b25f41
Ordenkrieger <OrdenKriegerEDR@googlemail.com>
2016-09-04 23:41:17 +0200

    Add MIDI support

--- ham.mk
+++ ham.mk
@@ -250,8 +250,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml
-
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml
+	
 # Data
 PRODUCT_PACKAGES += \
     librmnetctl
