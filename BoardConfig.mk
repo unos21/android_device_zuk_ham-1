2753f3471ea47472b1f4a7efdfa2721db2c13be5
OrdenKrieger <OrdenKriegerEDR@googlemail.com>
2016-12-22 19:02:51 +0100

    ham: Return static radio capability for sim 2
    * Our modem somehow returns incorrect radio
    capabilities for 2nd slot, that makes it
    stay on EDGE.
    * Change default network on 2nd slot to LTE

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -152,6 +152,7 @@ TARGET_POWERHAL_VARIANT := qcom
 
 # Radio
 TARGET_RIL_VARIANT := caf
+BOARD_RIL_CLASS := ../../../device/zuk/ham/ril/
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
