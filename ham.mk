e448d089574178809e8f4fc9133bdcfbeeeec432
sriram prakhya <sriramscience7@gmail.com>
2017-12-01 09:20:14 +0530

    Update ham.mk

--- ham.mk
+++ ham.mk
@@ -116,7 +116,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.1-service \
     fingerprint.msm8974
 
 # VNDK-SP:
